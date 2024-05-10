c122feec9dafb45c91e1fdca21956f973758c61c
Asadullah Ilyas <asadullah.ilyas@packagex.io>
2024-05-10 15:53:08 +0500

    Initial commit

--- HandyUtils/build.gradle.kts
+++ HandyUtils/build.gradle.kts
@@ -11,7 +11,7 @@ afterEvaluate {
                 from (components["release"])
                 groupId = "com.github.asadullahilyas"
                 artifactId = "HandyUtils"
-                version = "0.0.1"
+                version = "0.0.2"
             }
         }
     }
