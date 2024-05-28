de665bcccdb84f69fa7423762d2f51a5f6c6574c
Asadullah Ilyas <asadullah.ilyas@packagex.io>
2024-05-28 19:41:15 +0500

    More functions added in Context

--- HandyUtils/src/main/java/com/asadullah/handyutils/ContextExt.kt
+++ HandyUtils/src/main/java/com/asadullah/handyutils/ContextExt.kt
@@ -3,8 +3,15 @@ package com.asadullah.handyutils
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.util.DisplayMetrics
 import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.withContext
+import java.net.InetAddress
+import java.net.UnknownHostException
 
 fun Context.copyToClipboard(text: CharSequence, shouldShowToast: Boolean = false) {
     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
@@ -24,3 +31,36 @@ fun Context.getScreenHeight() = resources.displayMetrics.heightPixels
 
 fun Context.toDp(px: Int) = px / (resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
 fun Context.toPixel(dp: Int) = dp * (resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
+
+fun Context.isNetworkAvailable(): Boolean {
+    val connectivityManager = getSystemService(ConnectivityManager::class.java)
+    val networkCapabilities = connectivityManager.activeNetwork ?: return false
+    val actNw = connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+    return when {
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+        actNw.hasTransport(NetworkCapabilities.TRANSPORT_VPN) -> true
+        else -> false
+    }
+}
+
+suspend fun Context.isInternetAvailable(url: String = "www.stackoverflow.com"): Boolean {
+    if (isNetworkAvailable().not()) return false
+
+    val currentContext = currentCoroutineContext()
+
+    val result = withContext(Dispatchers.IO) {
+        try {
+            val address = InetAddress.getByName(url)
+            return@withContext !address.equals("")
+        } catch (e: UnknownHostException) {
+            e.printStackTrace()
+            return@withContext false
+        }
+    }
+
+    return withContext(currentContext) {
+        result
+    }
+}
