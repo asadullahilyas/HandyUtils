9de5c209daeb61f7c0647f89a28333d1c5757cc4
Asadullah Ilyas <asadullah.ilyas@packagex.io>
2024-10-10 17:25:48 +0500

    First CI/CD experiment

--- app/src/test/java/com/asadullah/handyutils/ExampleUnitTest.kt
+++ app/src/test/java/com/asadullah/handyutils/ExampleUnitTest.kt
@@ -10,8 +10,14 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun string_emptyStringTest_Correct() {
+        assertEquals(null, "  ".toNullIfEmptyOrBlank())
     }
 }
